a8dff5c0cab7b3545033e171d2b252af99d209c8
greyphilosophy <35280617+greyphilosophy@users.noreply.github.com>
2026-07-31 10:51:14 -0700

    Restore legacy public geometry helpers

--- birdcam.py
+++ birdcam.py
@@ -9,6 +9,7 @@ _optimized.compute_bird_crop = _framing.compute_bird_crop
 _optimized.advance_crop = _framing.advance_crop
 
 from birdcam_optimized import *  # noqa: F401,F403,E402
+from birdcam_legacy import advance_crop, compute_bird_crop  # noqa: F401,E402
 
 
 if __name__ == "__main__":
